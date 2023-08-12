358b09b886a3e4b2b4dede4cf879a6549557c570
Rodrigo Teixeira <you@example.com>
2023-08-12 18:40:26 -0300

    Fixing doGet Approach 10

--- assets/js/handleTikTokAPICallback.js
+++ assets/js/handleTikTokAPICallback.js
@@ -19,20 +19,13 @@ function handleAPICallback() {
 
     console.log(googleScriptUrl);
 
-    // Use fetch or other appropriate method to send data to the Google Apps Script
-    fetch(googleScriptUrl, {
-      method: 'POST'
-    }
-    )
-    .then(response => response.text())  // Get the response as text
-    .then(responseText => {
-      // Place the response text in the <p> tag with id 'api-response'
-      const apiResponseElement = document.getElementById('api-response');
-      if (apiResponseElement) {
-          apiResponseElement.textContent = responseText;
-      }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    const anchorElement = document.getElementById('api-response-button');
+    anchorElement.href = googleScriptUrl;
+
+    anchorElement.href = googleScriptUrl
+}
+
+function handleAuthorization() {
+  // Prompt the user for authorization
+  window.close();
 }
